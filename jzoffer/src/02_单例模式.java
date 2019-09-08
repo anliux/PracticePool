0706dac17a2144ac1ca68443867684de62147853
anliux <luckyxuan_1123@163.com>
2019-09-08 13:38:35 +0800

    init campus19

--- jzoffer/src/02_单例模式.java
+++ jzoffer/src/02_单例模式.java
@@ -26,3 +26,26 @@ public class Test2
 		return uniqueInstance2;
 	}
 }
+
+//懒汉式之并发访问
+class Single
+{
+	private Single(){}
+	private static Single s = null;
+	/*
+	并发访问有安全隐患，所以加入同步机制解决安全问题
+	但是，同步的出现却降低了效率。
+	提高效率：减少判断锁的次数，可以通过双重判断的方式。
+	*/
+	public static void getInstance()
+	{
+		if(s==null)
+		{
+			synchronized(Single.class){
+				if(s==null)
+					s = new Single();
+				return s;
+			}
+		}
+	}
+}
