e365e52f6b5cdc8c4866aed3a5b38a6db79e1e60
anliux <luckyxuan_1123@163.com>
2019-09-10 12:51:13 +0800

    bubblesort

---
+++ sort/src/BubbleSort.java
@@ -0,0 +1,39 @@
+//冒泡排序
+
+/*
+两层循环
+
+时间复杂度：O(n)
+空间复杂度：O(1)
+稳定
+内部排序
+*/
+
+public class BubbleSort  
+{
+	public static void bubbleSort(int[] arr)
+	{
+		int len = arr.length;
+		for(int i=0; i<len-1; i++)
+		{
+			for (int j=0; j<len-1-i; j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+		}
+
+	}
+	
+	public static void main(String[] args) 
+	{
+		int[] array = {12,34,32,56,18,42};
+		bubbleSort(array);
+		for(int i=0; i<array.length; i++)
+     		System.out.println("array[i]="+array[i]);
+	}
+}
