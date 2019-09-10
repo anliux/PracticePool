1aa895817cf3c3bf67d5bcb2f08ca7682fead5f4
anliux <luckyxuan_1123@163.com>
2019-09-10 17:04:39 +0800

    selectsort

--- sort/src/SelectSort.java
+++ sort/src/SelectSort.java
@@ -1,38 +1,37 @@
-//冒泡排序
+//选择排序
 
 /*
 两层循环
 
-时间复杂度：O(n^2): O(n)--O(n^2)
+时间复杂度：O(n^2): O(n^2)--O(n^2)
 空间复杂度：O(1)
-稳定
+不稳定
 内部排序
 */
 
-public class BubbleSort  
+public class SelectSort
 {
-	public static void bubbleSort(int[] arr)
+	public static void selectSort(int[] arr)
 	{
 		int len = arr.length;
-		for(int i=0; i<len-1; i++)
+		for (int i=0; i<len-1; i++)
 		{
-			for (int j=0; j<len-1-i; j++)
+			for (int j=i+1; j<len; j++)
 			{
-				if(arr[j]>arr[j+1])
+				if(arr[j]<arr[i])
 				{
 					int temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
+					arr[j] = arr[i];
+					arr[i] = temp;
 				}
 			}
 		}
-
 	}
 	
 	public static void main(String[] args) 
 	{
 		int[] array = {12,34,32,56,18,42};
-		bubbleSort(array);
+		selectSort(array);
 		for(int i=0; i<array.length; i++)
      		System.out.println("array[i]="+array[i]);
 	}
