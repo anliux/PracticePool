6b1aa1871e6867087e5b9374faf490ffbc58428c
anliux <luckyxuan_1123@163.com>
2020-02-11 14:59:54 +0800

    add lc 0053

---
+++ LeetCode/src/0053-maximum-subarray.java
@@ -0,0 +1,80 @@
+//0053-最大子序和
+//思路：暴力法；动态规划；贪心；分治
+
+
+
+//暴力法：57ms，击败了8.5%的用户（竟然没有超时..）
+/*
+    算法：
+	- 寻找所有的可能子序和，并求最大值
+    - 复杂度：时间O(N^2)，空间O(1)
+    - 首先定义并初始化max为数组第一个元素
+    - 按照一定规律遍历数组，比如从i开始遍历到末尾，不断取和，比较并取max，然后i++
+    - 两层循环：外循环控制开始的i值，内循环控制在i索引开始，形成的子序列
+*/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //定义max
+        int max = nums[0];
+
+        //外循环控制开始的i值，内循环控制在i索引开始，形成的子序列
+        for(int i=0; i<nums.length; i++){
+            //定义sum：每次外循环更新时，sum归0，重新开始统计
+            int sum = 0;
+            for(int j=i; j<nums.length; j++){
+                sum += nums[j];
+                if(sum>max)
+                    max = sum;
+            }
+        }
+
+        //返回最终的max
+        return max;
+    }
+}
+
+
+
+//贪心：1ms，击败99.96%的用户
+/*
+    算法：
+	- 从左向右遍历，一个个数累加；当sum<0，重新开始找子序列
+    - 所求最大值max可由if判断语句比较得到，也可由Math.max(i,j)直接求得
+    - sum初始化为0，小于0的sum说明累加后比初始值更小了
+    - "重新找"指的是：sum归0， 并从下一个元素起，重新累加
+	- 复杂度：时间O(n)，空间O(1)
+
+*/
+class Solution {
+    public int maxSubArray(int[] nums) {
+        //定义最大值max和累加和sum，其中max为最终返回值，sum为临时存储的累加和
+        int max = nums[0];
+        int sum = 0;
+
+        //循环遍历nums数组
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            
+            //max可有if判断语句比较求得，也可由Math函数直接求得
+            max = Math.max(sum, max); 
+
+            //判断sum的值：小于0时，sum归0， 并从下一个元素起，重新累加
+            if(sum<0){
+                sum = 0;
+            }
+        }
+
+        //返回循环结束后最终的max
+        return max;
+    }
+}
+
+
+
+//动态规划：
+/*
+
+
+
+*/
+
