8c0d1fa2c832b95d1ad9b0afd074d33ced9dfe18
Xuan Liu <anliux@tju.edu.cn>
2020-03-02 19:43:07 +0800

    Update 0005-longest-palindromic-substring.java

--- LeetCode/src/0005-longest-palindromic-substring.java
+++ LeetCode/src/0005-longest-palindromic-substring.java
@@ -115,4 +115,63 @@ class Solution {
 
 
 
-//中心扩散：
+//中心扩散：70ms，击败39.5%的用户
+/*
+  - 算法：
+      - 定义一个函数centerSpread，返回以left和right为中心扩散时的回文串
+        - while在left和right没有超过字符串s的首尾时循环；
+        - 循环体：if判断，相等时移动指针，不等时跳出循环；
+        - 跳出循环时，恰好满足 s.charAt(i) != s.charAt(j)，因此不能取i和j，而substring是左闭右开的，取(i+1,j)
+      - 主体函数：
+        - 边界值s.length()=0和1时为真，返回s；
+        - 定义最长子串res及其长度maxLens，并初始化，注意长度最小为1；
+        - for循环遍历中心位置i，直到i<s.length()-1，因为循环体中会调用奇数和偶数两种扩散方式，防止角标越界；
+        - 循环体中首先，分别定义奇数串和偶数串，并调用函数计算(i,i)和(i,i+1)作为中心的扩散结果；
+        - 循环体重其次：取奇数串和偶数串中最长的一个作为maxLensStr，并与res的长度maxLens比较，更长时更新res及maxLens；
+        - 循环结束后返回res。
+*/
+class Solution {
+    public String longestPalindrome(String s) {
+        //边界值判断
+        if(s.length() < 2)
+            return s;
+        
+        //定义最长子串res及其长度maxLens，并初始化。注意maxLens最小为1。
+        int maxLens = 1;
+        String res = s.substring(0,1);
+
+        //循环：从0开始遍历中心位置直到字符串最后一位的前一位(防止循环体中调用函数越界)
+        for(int i = 0; i < s.length()-1; i++){
+            String odd = centerSpread(s, i, i);
+            String even = centerSpread(s, i, i+1);
+            String better = (odd.length() > even.length()) ? odd : even;
+
+            //将maxStr与最长子串res比较，较大时，更新res信息
+            if(better.length() > maxLens){
+                res = better;
+                maxLens = better.length();
+            }
+        }
+
+        //循环结束后返回结果res
+        return res;
+    }
+
+    //定义中心扩散函数
+    private String centerSpread(String s, int left, int right){
+        //当left和right没有超出字符串s的首尾时，进行循环
+        while(left>=0 && right<s.length()){
+            //判断left和right对应的字符是否相等
+            if(s.charAt(left) == s.charAt(right)){
+                left--;
+                right++;
+            }else{
+                break;
+            }
+        }
+
+        //循环结束或break后，返回的是不符合条件的left和right，因此要退一步
+        //而截取字符串的函数substring是左闭右开的
+        return s.substring(left+1, right);
+    }
+}
