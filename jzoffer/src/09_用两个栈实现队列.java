bfdd7e45e1918d0a9c95c07e0bc2a454aee28de0
Xuan Liu <anliux@tju.edu.cn>
2020-03-19 21:48:16 +0800

    Update 09_用两个栈实现队列.java

--- jzoffer/src/09_用两个栈实现队列.java
+++ jzoffer/src/09_用两个栈实现队列.java
@@ -1,8 +1,10 @@
-/*
-09-用两个栈实现队列
+//09-用两个栈实现队列
 
-注意：栈相关的用法（创建、压栈弹栈、判空等）
-*/
+//注意：栈相关的用法（创建、压栈弹栈、判空等）
+
+
+
+//牛客代码：
 import java.util.Stack;
 
 public class Solution {
@@ -25,3 +27,45 @@ public class Solution {
 		return stack2.pop(); //当栈2非空时，直接弹出即可
     }
 }
+
+
+
+//LeetCode代码：70ms，击败71%的用户
+class CQueue {
+
+    //声明
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    public CQueue() {
+        //定义栈
+        stack1 = new Stack<Integer>();
+        stack2 = new Stack<Integer>();
+    }
+    
+    public void appendTail(int value) {
+        //栈1负责压栈：仅压栈，不做其他判断
+        stack1.push(value);
+    }
+    
+    public int deleteHead() {
+        //弹栈：对栈2判空，空则先把栈1都压入栈2，再弹；不空则直接从栈2弹栈。
+        if(stack1.isEmpty() && stack2.isEmpty())
+            return -1;
+
+        else if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+
+        return stack2.pop();
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
