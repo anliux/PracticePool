78e7cd717c01437e1453336c034c0bc188efa2cf
Xuan Liu <anliux@tju.edu.cn>
2020-03-24 18:59:51 +0800

    Update 17_打印从1到最大的n位数.java

--- jzoffer/src/17_打印从1到最大的n位数.java
+++ jzoffer/src/17_打印从1到最大的n位数.java
@@ -30,3 +30,29 @@ class Solution {
 
 
 //考虑大数问题：
+public void print1ToMaxOfNDigits(int n) {
+    if (n <= 0)
+        return;
+    char[] number = new char[n];
+    print1ToMaxOfNDigits(number, 0);
+}
+
+private void print1ToMaxOfNDigits(char[] number, int digit) {
+    if (digit == number.length) {
+        printNumber(number);
+        return;
+    }
+    for (int i = 0; i < 10; i++) {
+        number[digit] = (char) (i + '0');
+        print1ToMaxOfNDigits(number, digit + 1);
+    }
+}
+
+private void printNumber(char[] number) {
+    int index = 0;
+    while (index < number.length && number[index] == '0')
+        index++;
+    while (index < number.length)
+        System.out.print(number[index++]);
+    System.out.println();
+}
