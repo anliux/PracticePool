e783d000c8470eb3ab05f09b2c479c97a79cd889
Xuan Liu <anliux@tju.edu.cn>
2020-06-26 10:57:43 +0800

    Create 0264-ugly-number-ii.java

---
+++ LeetCode/src/0264-ugly-number-ii.java
@@ -0,0 +1,57 @@
+//0264 - 丑数II
+//思路：三指针 -- 设置数组保存找到的丑数，以空间换时间
+
+
+
+//三指针：3ms，击败84.44%的用户
+/*
+  - 面试思路：三指针法
+    - 分析：思路大致同改进思路。
+      - 基数是2，3，5，设置三个指针参数，分别作为三个基数的倍数，求乘积后取三者最小值作为下一个丑数。
+      - 求得最小值对应的指针，并更新。
+    - 算法：
+      - 初始化数组dp[]，三个指针i2，i3，i5;
+      - for循环：计算指定前n个丑数
+        - 定义并求三个乘积结果：乘积 = 前一个dp[i] * 某个指针；
+        - 用两个min找出三者中的最小值，作为下一个丑数存入dp[i]中；
+        - 比较并找出dp[i]对应的指针，并+1；
+      - 返回结果dp[n-1]。
+    - 注：if判断最小指针时，不能用if-else-if的格式，会报错；只能用if-if-if的格式
+      - 不知道为什么。
+*/
+public class Solution {
+    public int GetUglyNumber_Solution(int index) {
+        //这里默认了index是正整数
+        //结合丑数规律，考虑了前6项可以直接返回，稍微简化运算。
+        if(index<=6)
+            return index;
+        
+        //定义数组，第一个元素是1
+        int [] dp = new int[index];
+        dp[0]=1;
+        
+        //设置索引flag：均从第一个丑数1开始，例如dp[i2]，指向乘以对应因子的最近基数
+        int i2=0,i3=0,i5=0;
+        
+        //遍历:这里i是第几个丑数
+        for(int i=1;i<index;i++){
+            //更新nextFlag的值，并取其中的最小值
+            int next2=dp[i2]*2, next3=dp[i3]*3, next5=dp[i5]*5;
+            dp[i]=Math.min(next2,Math.min(next3,next5));
+            
+            //取到符合的最近邻丑数以后，更新对应的索引flag
+            //注：这里不能用if-else-if，只能if-if，否则报错。不知道为什么。
+            if(dp[i]==next2){
+                i2++;
+            }
+            if(dp[i]==next3){
+                i3++;
+            }
+            if(dp[i]==next5){
+                i5++;
+            }
+            
+        }
+        return dp[index-1];
+    }
+}
