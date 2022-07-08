265079318a2d62bc11ffbf110d92bee836cc6cd3
Xuan Liu <anliux@tju.edu.cn>
2022-07-08 17:52:43 +0800

    Update 10_3_跳台阶.java

--- jzoffer/src/10_3_跳台阶.java
+++ jzoffer/src/10_3_跳台阶.java
@@ -1,6 +1,24 @@
 //10.3 - 跳台阶
-//思路同斐波那契数列，注意首项和次项同斐波那契数列的区别
+//思路同斐波那契数列
+//注意：首项和次项同斐波那契数列的区别；根据题目对求和结果取模，或注意变量超出int范围
+//0ms，击败100%
+class Solution {
+    public int numWays(int n) {
+        if(n == 0 || n== 1)
+            return 1;
+        int a = 1, b = 1, sum = 0;//注意首项赋值
+        for(int i = 2; i <= n; i++){
+            sum = (a + b) % 1000000007;//对求和结果取模
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
+}
 
+
+
+//其他代码：
 public class Solution {
     public int JumpFloor(int target) {
         if(target<=0)
