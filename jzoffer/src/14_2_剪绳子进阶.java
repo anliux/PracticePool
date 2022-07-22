e38f4841eeb3c3cfd82e084558ad19200dfa0e4b
Xuan Liu <anliux@tju.edu.cn>
2022-07-22 20:10:57 +0800

    Update 14_2_剪绳子进阶.java

--- jzoffer/src/14_2_剪绳子进阶.java
+++ jzoffer/src/14_2_剪绳子进阶.java
@@ -2,23 +2,20 @@
 //思路：贪心
 
 
-
-//贪心：0ms，击败100%的用户
+//贪心：0ms/100%; 38.6 MB/12.69%
 class Solution {
     public int cuttingRope(int n) {
-        if(n <= 1)
-            return 0;
-        else if(n == 2)
+        if(n==2)
             return 1;
-        else if(n == 3)
+        else if(n==3)
             return 2;
-        
         long res = 1;
-        while(n > 4){
-            reas *= 3;
+        
+        while(n>4){//每次减去3，最终的乘积最大。长度小于等于4时，不再裁减，保持原长度乘积最大
+            res *= 3;
             res %= 1000000007;
             n -= 3;
         }
-        return (int)(res * n % 1000000007);
+        return (int) (res * n % 1000000007);
     }
 }
