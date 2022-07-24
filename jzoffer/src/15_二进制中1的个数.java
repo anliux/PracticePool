034fd9a3164448f423ea0095c8fde4d014ac4fde
Xuan Liu <anliux@tju.edu.cn>
2022-07-24 19:15:45 +0800

    Update 15_二进制中1的个数.java

--- jzoffer/src/15_二进制中1的个数.java
+++ jzoffer/src/15_二进制中1的个数.java
@@ -1,17 +1,20 @@
 //15 - 二进制中1的个数
 //设计二进制，考虑位运算。分析常规思路的不足，使用技巧解法。
 
-//技巧解法
+//技巧解法: 0ms/100%
 public class Solution {
-    public int NumberOf1(int n) {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
         int count = 0;
-        while(n!=0){//直接写n会报错，显示int不能转为boolean型
-            count++;
-            n = (n-1)&n;
+        while(n!=0){//需要判断是否为0，直接写n会报类型错误
+            count++;//n不为0，则至少有一个1，可直接自增1
+            n = (n-1) & n;//位运算规律：(n-1) & n，会让n最后一位的1变为0
         }
         return count;
     }
 }
+
+
 
 //Integer.bitCount(); -- 计算一个int，long类型在二进制下“1”的数量。
 //注：integer是lang包里的，不需要import。
