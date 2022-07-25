56844c805d40e54694c75b07a6d14df54a117b5b
Xuan Liu <anliux@tju.edu.cn>
2022-07-25 22:40:07 +0800

    Update 16_数值的整数次方.java

--- jzoffer/src/16_数值的整数次方.java
+++ jzoffer/src/16_数值的整数次方.java
@@ -1,38 +1,59 @@
 //16 - 数值的整数次方
 //实现pow函数
-//可直接调用；可使用次方的奇数偶数分解等式迭代求解
+
+
+
+//快速幂-递归：0ms/100%; 40.3 MB/94.62%
+class Solution {
+    public double myPow(double x, int n) {
+        //考虑边界值，防止负数取相反数溢出
+        long N = n;
+                
+        if(N>0)
+            return quickMul(x,N);
+        else
+            return (1.0 / quickMul(x,-N));
+        //return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+    }
+
+    public double quickMul(double x, long N){        
+        if(N==0)
+            return 1.0;
+        
+        double y = quickMul(x, N/2);
+                
+        if(N%2 == 0)
+            return y*y;
+        else
+            return y*y*x;
+        //return N % 2 == 0 ? y * y : y * y * x;
+    }
+}
+
+
+
+//评论区大佬改进：对负指数和奇偶数部分进行了统一
+class Solution {
+    public double myPow(double x, int n) {
+        //递归出口
+        if(n==0)
+            return 1.0;
+        else if(n==1)
+            return x;
+        else if(n==-1)
+            return 1.0/x;
+        
+        double half = myPow(x, n/2);
+        double mod = myPow(x, n%2);
+        return half * half * mod;
+    }
+}
+
+
 
 //直接调用pow函数
 public class Solution {
     public double Power(double base, int exponent) {
         return Math.pow(base,exponent);
-  }
-}
-
-//使用次方的分解等式迭代
-public class Solution {
-    public double Power(double base, int exponent) {
-        //判断指数的特殊情况：为0返回1，为1返回base
-		if(exponent==0)
-            return 1;
-        if(exponent==1)
-            return base;
-
-		//考虑指数为负时，设置一个标志，并在计算时取相反数，结果处取倒数
-        boolean isNegative = false;
-        if(exponent<0){
-            isNegative = true;
-            exponent = -exponent;
-        }
-
-		//根据指数的分解公式进行迭代
-        double pow = Power(base*base, exponent/2);
-        //判断指数的奇偶性，奇数时需求额外乘以一次底数
-		if(exponent%2 != 0){
-            pow = pow * base;
-        }
-
-		//返回时进行判断指数是否为负数，如果为负，取倒数；否则，直接返回。
-        return isNegative?(1/pow):pow;
   }
 }
