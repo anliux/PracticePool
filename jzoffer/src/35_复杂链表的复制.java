7f94d05cc2c0792dda48e06a16b4b19abac86e95
Xuan Liu <anliux@tju.edu.cn>
2022-08-08 17:28:38 +0800

    Update 35_复杂链表的复制.java

--- jzoffer/src/35_复杂链表的复制.java
+++ jzoffer/src/35_复杂链表的复制.java
@@ -50,4 +50,60 @@ class Solution {
 
 
 
-//拼接拆分：
+//拼接拆分：0ms/100%; 40.9 MB/69.94%
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        //判空
+        if(head==null)
+            return null;
+        
+        //初始化当前结点：指向原链表的头结点
+        Node cur = head;
+
+        //遍历：复制各节点，并构建拼接链表
+        while(cur!=null){
+            Node temp = new Node(cur.val);//新建结点
+            temp.next = cur.next;
+            cur.next = temp;//temp结点插入到cur与cur.next之间
+            cur = temp.next;
+        }
+
+        //cur结点复位到head结点
+        cur = head;
+
+        //遍历：构建各新节点的 random 指向
+        while(cur!=null){
+            if(cur.random != null)//要用到cur.random.next，加个判断
+                cur.next.random = cur.random.next;
+            cur = cur.next.next;
+        }
+
+        //拆分原链表与新链表
+        cur = head.next;//新链表
+        Node pre = head, res = head.next;
+        while(cur.next != null){
+            pre.next = pre.next.next;
+            cur.next = cur.next.next;
+            pre = pre.next;
+            cur = cur.next;
+        }
+
+        //补充原链表，并返回结果
+        pre.next = null;
+        return res;
+    }
+}
