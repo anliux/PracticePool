e34edc83eefc058415f414c1c6e3f19815075557
Xuan Liu <anliux@tju.edu.cn>
2022-08-13 00:25:13 +0800

    Update 0053-maximum-subarray.java

--- LeetCode/src/0053-maximum-subarray.java
+++ LeetCode/src/0053-maximum-subarray.java
@@ -1,9 +1,9 @@
-//0053-最大子序和
-//思路：暴力法；动态规划；贪心；分治
+//53. 最大子数组和
+//思路：暴力法；贪心；动态规划；分治
 
 
 
-//暴力法：57ms，击败了8.5%的用户（竟然没有超时..）
+//暴力法：超时了，最后一个很长的测试用例没有通过
 /*
   算法：
   - 寻找所有的可能子序和，并求最大值
@@ -35,7 +35,7 @@ class Solution {
 
 
 
-//贪心：1ms，击败99.96%的用户
+//贪心：0ms/100%; 48.4 MB/7.10%
 /*
   算法：
   - 从左向右遍历，一个个数累加；当sum<0，重新开始找子序列
@@ -43,7 +43,6 @@ class Solution {
     - sum初始化为0，小于0的sum说明累加后比初始值更小了
     - "重新找"指的是：sum归0， 并从下一个元素起，重新累加
   - 复杂度：时间O(n)，空间O(1)
-
 */
 class Solution {
     public int maxSubArray(int[] nums) {
@@ -55,8 +54,10 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             sum += nums[i];
             
-            //max可有if判断语句比较求得，也可由Math函数直接求得
-            max = Math.max(sum, max); 
+            //max可由if判断语句比较求得，也可由Math函数直接求得
+            //max = Math.max(sum, max); 
+            if(sum > max)
+                max = sum;
 
             //判断sum的值：小于0时，sum归0， 并从下一个元素起，重新累加
             if(sum<0){
@@ -71,7 +72,7 @@ class Solution {
 
 
 
-//动态规划：1ms，击败99.96%的用户；48.6M，击败5%的用户
+//动态规划：1 ms/41.19%; 47.9 MB/73.23%
 /*
     算法：
     - 复杂度：时间O(n), 空间O(n)，可优化到O(1)
@@ -96,7 +97,7 @@ class Solution {
     }
 }
 
-//动态规划改进空间复杂度：经测试，改进后与改进前几乎没有差别
+//动态规划改进空间复杂度：0 ms/100.00%; 48.2 MB/24.54%
 class Solution {
     public int maxSubArray(int[] nums) {
         //定义结果result和int数组dp，并初始化为nums数组的第一个元素
@@ -110,6 +111,18 @@ class Solution {
         }
 
         return result;
+    }
+}
+
+//贴一个动态规划k神的优雅写法：用原数组存储dp结果
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for(int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(res, nums[i]);
+        }
+        return res;
     }
 }
 
