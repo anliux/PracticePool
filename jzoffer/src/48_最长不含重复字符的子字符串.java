775424af4337bc5b334c1b6734e91a488ee8dfa3
Xuan Liu <anliux@tju.edu.cn>
2022-08-17 16:36:45 +0800

    Update 48_最长不含重复字符的子字符串.java

--- jzoffer/src/48_最长不含重复字符的子字符串.java
+++ jzoffer/src/48_最长不含重复字符的子字符串.java
@@ -3,44 +3,31 @@
 
 
 
-//滑动窗口：6ms，击败88.69%
-/*
-   - 算法：
-      - 初始化结果变量res=0；
-      - 定义哈希表map：存储 (k, v)，其中 key 值为字符，value 值为字符位置 +1，加 1 表示从字符位置后一个才开始不重复
-      - for循环遍历：初始化 开始位置为 start=0，结束位置为 end=0
-        - 初始化字符：`char chs = s.charAt(end);` -- 以end对应的字符为基准进行遍历
-        - map.containsKey -- 判断chs是否已经存在窗口中
-          - 当存在时，更新start的值为start和map.get(key)中的max；
-        - 更新res为res和end-start+1中的最大值；
-        - map.put(key, value) -- 其中，key表示end对应字符，value为end+1，表示下一个无重复位置，方便get的时候更新start的值。
-      - 返回结果变量res。
-*/
+//滑动窗口：4 ms/91.74%; 41.7 MB/32.55%
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        //定义结果变量res
-        int res = 0;
+        //滑动窗口：左开右闭
 
-        //定义哈希表：key表示end索引对应的字符，value表示end+1
-        Map<Character, Integer> map = new HashMap<>();
+        //初始化：结果res，左指针left
+        int res = 0, left = -1;
 
-        //滑动窗口：for循环遍历
-        for(int start = 0, end = 0; end < s.length(); end++){
-            //定义变量chs，并获取end索引值对应的字符
-            char chs = s.charAt(end);
+        //初始化哈希表map
+        HashMap<Character, Integer> map = new HashMap<>();
 
-            //判断map中是否存在字符chs：若存在，更新start的值
-            if(map.containsKey(chs))
-                start = Math.max(start, map.get(chs));
-            
-            //更新res
-            res = Math.max(res, end - start + 1);
+        //右指针遍历：
+        for(int right = 0; right < s.length(); right++){
+            //如果对应字符已包含在哈希表中，则更新左指针
+            if(map.containsKey(s.charAt(right)))
+                left = Math.max(left, map.get(s.charAt(right)));
 
-            //将窗口的右侧字符与对应索引值存入map
-            map.put(chs, end + 1);
+            //每次更新right对应字符的最新位置到哈希表
+            map.put(s.charAt(right), right);
+
+            //每次更新res
+            res = Math.max(res, right-left);
         }
 
-        //返回结果变量res
+        //返回结果
         return res;
     }
 }
