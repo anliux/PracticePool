7701157d8011144e33106ef1b1c243939db4992e
Xuan Liu <anliux@tju.edu.cn>
2022-08-18 16:42:32 +0800

    Update 0160-intersection-of-two-linked-lists.java

--- LeetCode/src/0160-intersection-of-two-linked-lists.java
+++ LeetCode/src/0160-intersection-of-two-linked-lists.java
@@ -2,14 +2,7 @@
 //思路：暴力法；哈希表法；双指针法
 
 
-
-//双指针法：1ms，击败100%的用户
-/*
-    - 思路：链表A：a+c, 链表B : b+c. a+c+b+c = b+c+a+c 。则会在第二个c起点相遇。若不相交，a+b = b+a 。因此相遇处是NULL（尾）
-    - 算法：判空；新建结点；循环：两结点移动，直到相等。
-    - 注：没有公共时，最后都指向null，此时相等，退出循环。因此不会死循环。
-    - 代码使用了三目运算符，很巧妙。
-*/
+//双指针法：1 ms/98.48%; 44.6 MB/21.59%
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -21,22 +14,23 @@
  *     }
  * }
  */
-public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        //判空
+class Solution {
+    ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //判空：
         if(headA == null || headB == null)
             return null;
-        
-        //新建两指针结点
+
+        //初始化两个指针：
         ListNode pa = headA, pb = headB;
 
-        //循环，直到pa和pb相等
-        while(pa != pb){
-            pa = (pa == null) ? headB : pa.next;
-            pb = (pb == null) ? headA : pb.next;
+        //循环：a+c+b = b+c+a, 当两指针相遇时退出
+        while(pa != pb){//遍历到本链表的尾结点时，继续从另一链表头结点开始遍历
+            pa = (pa!=null)? pa.next : headB ;
+            pb = (pb!=null)? pb.next : headA ;
         }
 
-        //返回任一
+        //返回结果：任一指针即可，有共同结点和无共同结点均符合
         return pa;
     }
 }
+
