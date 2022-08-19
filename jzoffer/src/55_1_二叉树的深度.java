37b7b43a86e4a77a510ce8a4cbe93fbfbf68a815
Xuan Liu <anliux@tju.edu.cn>
2022-08-19 18:27:44 +0800

    Update 55_1_二叉树的深度.java

--- jzoffer/src/55_1_二叉树的深度.java
+++ jzoffer/src/55_1_二叉树的深度.java
@@ -1,6 +1,8 @@
 //55.1 - 二叉树的深度
-//递归。从根结点开始分析不同情况下二叉树的深度，寻找规律。
 
+
+//递归。从根结点开始分析不同情况下二叉树的深度，寻找规律。
+//0 ms/100.00%; 40.7 MB/98.09%
 public class Solution {
     public int TreeDepth(TreeNode root) {
         return root==null?0:1+Math.max(TreeDepth(root.left),TreeDepth(root.right));
