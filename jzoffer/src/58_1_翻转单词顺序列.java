4e3f4f978bfb3cffeea7378447966fdb65d309a7
Xuan Liu <anliux@tju.edu.cn>
2022-08-25 21:25:43 +0800

    Update 58_1_翻转单词顺序列.java

--- jzoffer/src/58_1_翻转单词顺序列.java
+++ jzoffer/src/58_1_翻转单词顺序列.java
@@ -1,53 +1,67 @@
 //58.1 - 翻转单词顺序列
-//思路：两次翻转；一次翻转
+//思路：库函数；一次翻转；两次翻转；
 
 
+//库函数+一次翻转：4 ms/41.20%; 41.1 MB/90.32%
+//用了很多库函数，没体现出算法功底
+class Solution {
+    public String reverseWords(String s) {
+        //拆分-倒序组合-转化为String返回
 
-//单一空格处理：
-public class Solution {
-    public String ReverseSentence(String str) {
-        int n = str.length();
+        //拆分
+        String[] strs = s.trim().split(" +");
 
-        //首先将str转为char数值，方便后续操作
-        char[] chars = str.toCharArray();
-        
-        //定义指针，方便遍历以及寻找空格
-        int i = 0, j = 0;
-        
-        //注：先翻转单词，后翻转整个str。都一样。
-        
-        //循环，直到尾指针j遍历到末尾
-        while(j<=n){//为了与j-1统一，循环条件设为包含=
-            //注意：char型，故空格需要用单引号而不是双引号
-            if(j==n || chars[j]==' '){//当j遍历到末尾或者遇到空格时，翻转i到j-1的内容
-                reverse(chars, i, j-1);
-                i = j+1;
-            }
-            j++;//j继续自增
+        //初始化可变字符串
+        StringBuilder sb = new StringBuilder();
+
+        //倒序遍历
+        for(int i = strs.length-1; i >= 0; i--){
+            sb.append(strs[i]).append(" ");
         }
-        
-        //翻转整个str
-        reverse(chars, 0, n-1);
-        
-        //将char数组转为String并返回
-        //构造函数 String(char[] value): 分配一个新的String，使其表示字符数组参数中当前包含的字符序列。
-        return new String(chars);
+
+        //转换后返回：如果遍历没有判定，这里转换后修剪掉多加的空格
+        return sb.toString().trim();
     }
-    
-    private void reverse(char[] c, int i, int j){
-        while(i<j)
-            swap(c, i++, j--);
-    }
-    
-    private void swap(char[] c, int i, int j){
-        char temp = c[i];
-        c[i] = c[j];
-        c[j] = temp;
+}
+
+
+//双指针+一次翻转：没用split(), 手动空格处理：2 ms/74.62%; 41.3 MB/60.90%
+class Solution {
+    public String reverseWords(String s) {
+        //双指针步骤：一次翻转，手动处理空格
+
+        //初始化：两指针和可变字符串
+        int i = s.length() - 1;
+        int j = i;
+        StringBuilder sb = new StringBuilder();
+
+        //倒序遍历：
+        while(i>=0){
+            //小指针向左找第一个空格
+            while(i>=0 && s.charAt(i)!=' ')//注意此处空格用单引号，下同
+                i--;
+
+            //将大小指针之间的子串添加到sb
+            sb.append(s.substring(i+1,j+1)).append(" ");
+
+            //小指针继续向左找第一个非空格：越过所有空格
+            while(i>=0 && s.charAt(i)==' ')
+                i--;
+
+            //更新大指针
+            j=i;
+        }
+
+        //返回结果：转换并返回，记得再次枝剪避免前后多空格
+        return sb.toString().trim();
     }
 }
 
 
 
+
+
+//其他：
 //两次翻转+多空格处理：13ms，击败20%的用户
 /*
 - 算法1：两次翻转
@@ -107,37 +121,6 @@ class Solution {
 
 
 
-//可变字符串+一次翻转：6ms，击败42.5%的用户
-/*
-- 算法2：一次翻转
-    - 首先处理空格：剪去所有空格后，将字符串数组重新用一个空格连起来；
-    - 定义可变字符串StringBuilder；
-    - 从后遍历字符串数组：如果不是第一个元素，则额外添加空格，否则，只添加元素本身；
-    - 将可变字符串转为String类型，并返回。
-    - 注：简单多了，但是用了很多库函数吧。
-*/
-class Solution {
-    public String reverseWords(String s) {
-        //首先处理空格，然后再用一个空格将字符串拼起来
-        //注意：split(" +")可以剪去多个空格
-        String [] words = s.trim().split(" +");
-
-        //定义可变字符串：
-        StringBuilder sb = new StringBuilder();
-
-        //遍历字符串，并添加空格
-        for(int i = words.length - 1; i >= 0; i--){
-            if(i == 0)
-                sb.append(words[i]);
-            else
-                sb.append(words[i]).append(" ");
-        }
-        
-        return sb.toString();
-    }
-}
-
-
 
 //直接一次翻转：感觉没啥问题但是超出时间限制。
 class Solution {
@@ -157,5 +140,50 @@ class Solution {
         }
         
         return String.join(" ", words);
+    }
+}
+
+
+
+//单一空格处理：
+public class Solution {
+    public String ReverseSentence(String str) {
+        int n = str.length();
+
+        //首先将str转为char数值，方便后续操作
+        char[] chars = str.toCharArray();
+        
+        //定义指针，方便遍历以及寻找空格
+        int i = 0, j = 0;
+        
+        //注：先翻转单词，后翻转整个str。都一样。
+        
+        //循环，直到尾指针j遍历到末尾
+        while(j<=n){//为了与j-1统一，循环条件设为包含=
+            //注意：char型，故空格需要用单引号而不是双引号
+            if(j==n || chars[j]==' '){//当j遍历到末尾或者遇到空格时，翻转i到j-1的内容
+                reverse(chars, i, j-1);
+                i = j+1;
+            }
+            j++;//j继续自增
+        }
+        
+        //翻转整个str
+        reverse(chars, 0, n-1);
+        
+        //将char数组转为String并返回
+        //构造函数 String(char[] value): 分配一个新的String，使其表示字符数组参数中当前包含的字符序列。
+        return new String(chars);
+    }
+    
+    private void reverse(char[] c, int i, int j){
+        while(i<j)
+            swap(c, i++, j--);
+    }
+    
+    private void swap(char[] c, int i, int j){
+        char temp = c[i];
+        c[i] = c[j];
+        c[j] = temp;
     }
 }
