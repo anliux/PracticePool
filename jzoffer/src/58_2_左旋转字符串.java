d5881cd74acbce71f1590387bd84f7c8719aafbf
Xuan Liu <anliux@tju.edu.cn>
2022-08-28 00:18:47 +0800

    Update 58_2_左旋转字符串.java

--- jzoffer/src/58_2_左旋转字符串.java
+++ jzoffer/src/58_2_左旋转字符串.java
@@ -1,18 +1,33 @@
 //58.2 - 左旋转字符串
-//思路：三次翻转
+//思路：库函数；列表遍历；三次翻转
+
+
+//库函数：0 ms/100.00%; 41.4 MB/48.88%
+class Solution {
+    public String reverseLeftWords(String s, int n) {
+        return s.substring(n,s.length()) + s.substring(0,n);
+    }
+}
 
 
 
-//三次翻转：2ms，击败29%的用户
-/*
- - 算法：
-    - 定义翻转函数；
-    - 边界值判断；（如果题目规定边界值，可简化或省略）
-    - 将字符串转为字符数组；
-    - 整体翻转以及分别翻转两段字符串；
-    - 重新构建字符串并返回。
-*/
-//注意：仍然是先转成字符数组进行操作，最后再转成String返回
+//列表遍历：3 ms/44.07%; 41.6 MB/23.47%
+class Solution {
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = n; i < s.length(); i++){
+            sb.append(s.charAt(i));
+        }
+        for(int i = 0; i < n; i++){
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
+
+
+
+//三次翻转：2ms/52.22%; 41.5 MB/32.04%
 class Solution {
     public String reverseLeftWords(String s, int n) {
         //边界值判断：如果n大于字符串长度则直接返回原字符串。不过此处题目规定了取值范围。
@@ -39,14 +54,5 @@ class Solution {
             left++;
             right--;
         }
-    }
-}
-
-
-
-//用substring偷懒: 0ms，击败100%的用户
-class Solution {
-    public String reverseLeftWords(String s, int n) {
-        return s.substring(n, s.length()) + s.substring(0, n);
     }
 }
