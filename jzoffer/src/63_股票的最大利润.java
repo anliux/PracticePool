648242a5f3875d4fdcb28f20fdae00302e6f63af
Xuan Liu <anliux@tju.edu.cn>
2023-05-24 15:53:23 +0800

    Update 63_股票的最大利润.java

--- jzoffer/src/63_股票的最大利润.java
+++ jzoffer/src/63_股票的最大利润.java
@@ -1,9 +1,10 @@
 //63-股票的最大利润
-//思路：暴力法；一次遍历
+//思路：暴力法；一次遍历; 动态规划
 
 
 
-//暴力法：326ms，击败9.7%的用户
+//暴力法：243 ms/6.93%; 43.2 MB/5.32%
+//可以AC，但是太慢了
 class Solution {
     public int maxProfit(int[] prices) {
         //定义max，并初始化为0（不存在时返回0，即范围内最小值为0）
@@ -23,12 +24,34 @@ class Solution {
 
 
 
-//一次遍历：1ms，击败99%的用户
-/*
-    思路：找到最小的谷之后的最大的峰
-    - 维持两个变量 - minprice 和 maxprofit，它们分别对应迄今为止所得到的最小的谷值和最大的利润（卖出价格与最低价格之间的最大差值）。
-    - 算法：定义两个最值并初始化；遍历：先小后大 - 当[i]值更小时，更新min，否则，比较[i]-min与max的大小并更新max；返回max。
-*/
+//一次遍历：1ms/51.82%; 43 MB/10.97%
+//思路：找到最小的谷之后的最大的峰; 注意初始化值和循环开始值
+class Solution {
+    public int maxProfit(int[] prices) {
+        //判空
+        if(prices == null || prices.length < 2)
+            return 0;
+
+        //初始化两个最值
+        int min = prices[0];
+        int maxDiff = 0;
+
+        //循环：一次遍历，并得到最终的max
+        for(int i = 1; i < prices.length; i++){
+            //每次首先更新min
+            min = (prices[i-1]<min) ? prices[i-1] : min;
+
+            //加入后一个元素: 对比当前差值与最大差值
+            if(prices[i]-min > maxDiff)
+                maxDiff = prices[i] - min;
+        }
+
+        //返回最值max
+        return maxDiff;
+    }
+}
+
+//当没有判空时：初始化要用到Integer的最值
 class Solution {
     public int maxProfit(int[] prices) {
         //定义最值，并初始化为合适的值。
