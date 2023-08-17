ed83be66bf6c1a4c54789f75ee56fe40eea5806f
Xuan Liu <anliux@tju.edu.cn>
2023-08-17 17:15:51 +0800

    Update 68.1_二叉搜索树的最近公共祖先.java
    no.68.1: added the code of 迭代优化版

--- jzoffer/src/68.1_二叉搜索树的最近公共祖先.java
+++ jzoffer/src/68.1_二叉搜索树的最近公共祖先.java
@@ -32,8 +32,36 @@ class Solution {
 
 
 
-//迭代优化版：
+//迭代优化版：6ms/39.57%; 40.70mb/88.21%
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //先取p,q两节点的最值
+        int max = Math.max(p.val, q.val);
+        int min = Math.min(p.val, q.val);
+        
+        //遍历直到root为null
+        while(root != null){
+            if(min > root.val)    //p,q都在右子树：遍历至右子节点
+                root = root.right;
+            else if(max < root.val)   //p,q都在左子树：遍历至左子节点
+                root = root.left;
+            else    //否则，p,q 在root节点两侧时，符合要求，退出循环
+                break;
+        }
 
+        //返回结果节点
+        return root;
+    }
+}
 
 
 
