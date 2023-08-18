4102d150420ba4088a44c99f078a9459f24a02cc
Xuan Liu <anliux@tju.edu.cn>
2023-08-18 11:24:04 +0800

    Update 68.1_二叉搜索树的最近公共祖先.java
    no.68.1: added the code of 递归

--- jzoffer/src/68.1_二叉搜索树的最近公共祖先.java
+++ jzoffer/src/68.1_二叉搜索树的最近公共祖先.java
@@ -68,4 +68,23 @@ class Solution {
 
 
 
-//递归：
+//递归：5ms/100%; 41.20mb/5.04%
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p.val > root.val && q.val > root.val)
+            return lowestCommonAncestor(root.right, p, q);
+        else if(p.val < root.val && q.val < root.val)
+            return lowestCommonAncestor(root.left, p, q);
+        else
+            return root;
+    }
+}
